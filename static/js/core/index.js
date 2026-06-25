c53eac2cac85e825806b257d8c214fc6748197c3
Vladtopchik <vladkyriniq@gmail.com>
2026-06-25 19:03:51 +0300

    added animation on index

--- static/js/core/index.js
+++ static/js/core/index.js
@@ -1,13 +1,68 @@
-const aboutAnimationContainer = document.getElementsByClassName("about-animation");
+const aboutAnimationFirstContainer = document.getElementById("animation-first-element");
+const aboutAnimationMainContainer = document.getElementById("animation-container");
 
-function createAnimation(container, path) {
-    return lottie.loadAnimation({
+function createAnimation(container, animationData, startFrame=0) {
+    const anim =  lottie.loadAnimation({
         container,
         renderer: "svg",
         loop: true,
-        autoplay: true,
-        path
+        autoplay: false,
+        animationData
     });
+
+    anim.goToAndPlay(startFrame, true);
+
+    return anim;
 }
 
-createAnimation(aboutAnimationContainer[0], aboutAnimationPath);
+
+
+const item = sessionStorage.getItem("animation");
+let animation;
+let initialAnimation;
+
+if (item === null) {
+    fetch(aboutAnimationPath)
+        .then(res => res.text())
+        .then(text => {
+            sessionStorage.setItem("animation", text);
+            animation = JSON.parse(text);
+            initialAnimation = createAnimation(aboutAnimationFirstContainer, animation);
+            createAnimationsToFit();
+        });
+} else {
+    animation = JSON.parse(item);
+    initialAnimation = createAnimation(aboutAnimationFirstContainer, animation);
+    createAnimationsToFit();
+}
+
+function getFitCount(mainContainer, item) {
+    const mainWidth = mainContainer.offsetWidth;
+
+    const style = window.getComputedStyle(item);
+    const margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+
+    const itemTotalWidth = item.offsetWidth + margin;
+
+    if (itemTotalWidth <= 0) return 0;
+
+    return Math.ceil(mainWidth / itemTotalWidth);
+}
+
+function createAnimationsToFit() {
+    const containersCount = aboutAnimationMainContainer.children.length;
+    const maxContainersCount = getFitCount(aboutAnimationMainContainer, aboutAnimationFirstContainer);
+
+    const containersToFit = maxContainersCount - containersCount;
+
+    if (containersToFit <= 0) return;
+
+    for (let i = 0; i < containersToFit; i++) {
+        const el = aboutAnimationFirstContainer.cloneNode(false);
+        aboutAnimationMainContainer.appendChild(el);
+        createAnimation(el, animation, initialAnimation.currentFrame);
+    }
+
+}
+
+window.addEventListener('resize', createAnimationsToFit);
